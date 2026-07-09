739cd63d82b1159420a4cd4fc8b3487af257c45c
Arão Farias <arao.farias@sefaz.ma.gov.br>
2026-07-09 07:54:00 -0300

    feat(frontend): página 404, loading global, breadcrumb e animação tela vazia

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { EventList } from './features/events/pages/event-list/event-list';
 import { EventForm } from './features/events/pages/event-form/event-form';
 import { EventDetail } from './features/events/pages/event-detail/event-detail';
+import { NotFound } from './shared/not-found/not-found';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'events', pathMatch: 'full' },
@@ -9,4 +10,5 @@ export const routes: Routes = [
   { path: 'events/new', component: EventForm },
   { path: 'events/:id/edit', component: EventForm },
   { path: 'events/:id', component: EventDetail },
+  { path: '**', component: NotFound },
 ];
